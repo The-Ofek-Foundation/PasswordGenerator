1bd4b9b736c8807cdf8f75972afbd1e97be641a8
Ofek Gila <ofek@theofekfoundation.org>
2016-11-29 18:22:50 -0800

    Remove jquery and add whitelist

--- PasswordGenerator.js
+++ PasswordGenerator.js
@@ -1,17 +1,31 @@
-$("#generate-form").submit(function () {
-	var password = "", length = $("input[name=length]").val(), illegal = $("input[name=illegal]").val(), char;
+const LOWERCASE_ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+const UPPERCASE_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const NUMBERS = '1234567890';
+
+function generatePassword() {
+	var password = "",
+	    length = getInputValue('length'),
+	    illegal = getInputValue('blacklist'), char;
+	var whitelist = getInputValue('whitelist');
 	for (var i = 0; i < length; i++) {
 		do {
-			char = String.fromCharCode(Math.random() * 95 + 32 | 0);
+			if (whitelist.length > 0)
+				char = whitelist.charAt(parseInt(Math.random() *
+					whitelist.length));
+			else char = String.fromCharCode(Math.random() * 95 + 32 | 0);
 		}	while (illegal.indexOf(char) !== -1);
 		password += char;
 	}
-	$("#password").val(password);
+	getElemId('password').value = password;
 	return false;
+}
+
+getElemName('alphanumeric').addEventListener('click', function() {
+	setInputValue('whitelist', LOWERCASE_ALPHABET + UPPERCASE_ALPHABET + NUMBERS);
 });
 
-$("#copy-button").click(function (e) {
-	copyToClipboard(document.getElementById('password').value);
+getElemId('copy-button').addEventListener('click', function () {
+	copyToClipboard(getElemId('password').value);
 });
 
 function copyToClipboard(text) {
